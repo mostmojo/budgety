6ca577830407c7b0912e59bce77485fa43b749e9
Jacob Papageorgiou <jacob@ten4design.co.uk>
2019-06-18 15:29:02 +0100

    Add add__btn to DOMStrings var to make more scalable

--- app.js
+++ app.js
@@ -10,7 +10,8 @@ var UIController = (function() {
     var DOMStrings = {
         inputType: '.add__type',
         inputDescription: '.add__description',
-        inputValue: '.add__value'
+        inputValue: '.add__value',
+        inputBtn: '.add__btn'
     };
 
     return {
@@ -20,6 +21,10 @@ var UIController = (function() {
                 description: document.querySelector(DOMStrings.inputDescription).value,
                 value: document.querySelector(DOMStrings.inputValue).value
             }
+        },
+
+        getDOMStrings: function() {
+            return DOMStrings; // this exposes the private DOMStrings object into the public
         }
     }
 })();
@@ -27,21 +32,21 @@ var UIController = (function() {
 
 // GLOBAL APP CONTROLLER
 var AppController = (function(budgetCtrl, UICtrl) { // these are params
+
+    var DOM = UICtrl.getDOMStrings(); // can access the public object values from var UIController
+
     var ctrlAddItem = function() {
         // 1. Get the field input data
         var input = UICtrl.getInput(); // can access the public object values from var UIController
         console.log(input);
 
         // 2. Add the item to budget controller
-
         // 3. Add the item to UI
-
         // 4. Calculate the budget
-
         // 5. Display the budget to UI
     }
 
-    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem)
+    document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem)
 
     document.addEventListener('keypress', function(event) {
         if (event.key === "Enter") {
