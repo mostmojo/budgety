ff3fafecc79576b2e3f5ba52f586715246dffd84
Jacob Papageorgiou <jacob@ten4design.co.uk>
2019-06-20 12:50:23 +0100

    Add a getBudget reset when app is initialized to reset all the budget UI

--- app.js
+++ app.js
@@ -207,6 +207,12 @@ var AppController = (function(budgetCtrl, UICtrl) {
 	return {
 		init: function() {
 			console.log('Application has started.');
+			UICtrl.displayBudget({
+				budget: 0,
+				totalInc: 0,
+				totalExp: 0,
+				percentage: -1
+			});
 			setupEventListeners();
 		}
 	};
