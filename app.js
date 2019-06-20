55c474a20c9cd4bc3e6ec40951451207583579d9
Jacob Papageorgiou <jacob@ten4design.co.uk>
2019-06-20 12:45:50 +0100

    Add if condition to prevent negative percentage displays

--- app.js
+++ app.js
@@ -149,7 +149,12 @@ var UIController = (function() {
 			document.querySelector(DOMStrings.budgetLabel).textContent = obj.budget;
 			document.querySelector(DOMStrings.incomeLabel).textContent = obj.totalInc;
 			document.querySelector(DOMStrings.expensesLabel).textContent = obj.totalExp;
-			document.querySelector(DOMStrings.percentageLabel).textContent = obj.percentage;
+
+			if (obj.percentage > 0) {
+				document.querySelector(DOMStrings.percentageLabel).textContent = obj.percentage + '%';
+			} else {
+				document.querySelector(DOMStrings.percentageLabel).textContent = '---';
+			}
 		},
 
 		getDOMStrings: function() {
