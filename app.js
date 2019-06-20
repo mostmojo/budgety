d7f70405a14363c7a090b98ac45d12fab4fe61fb
Jacob Papageorgiou <jacob@ten4design.co.uk>
2019-06-20 17:05:36 +0100

    Use JS split method to separate a value into two elements

--- app.js
+++ app.js
@@ -122,10 +122,10 @@ var UIController = (function() {
 			// Create HTML string with placeholder text
 			if (type === 'inc') {
 				element = DOMStrings.incomeContainer;
-				html = '<div class="item clearfix" id="income-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><ion-icon name="close"></ion-icon></button></div></div></div>';
+				html = '<div class="item clearfix" id="inc-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><ion-icon name="close"></ion-icon></button></div></div></div>';
 			} else if (type === 'exp') {
 				element = DOMStrings.expensesContainer;
-				html = '<div class="item clearfix" id="expense-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><ion-icon name="close"></ion-icon></div></div>';
+				html = '<div class="item clearfix" id="exp-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><ion-icon name="close"></ion-icon></div></div>';
 			}
 			// Replace the placeholder text with some actual data
 			newHtml = html.replace('%id%', obj.id);
@@ -208,11 +208,14 @@ var AppController = (function(budgetCtrl, UICtrl) {
 	};
 
 	var ctrlDeleteItem = function(event) {
-		var itemID;
+		var itemID, splitID, type, ID;
 
 		itemID = (event.target.parentNode.parentNode.parentNode.parentNode.id);
 		if (itemID) {
-
+			// inc-1 => will be `.split` into ["inc", "1"] w/ this JS method
+			splitID = itemID.split('-');
+			type = splitID[0];
+			ID = splitID[1];
 		}
 	};
 
